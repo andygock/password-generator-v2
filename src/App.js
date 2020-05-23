993d5a8c1637b5649a106aed7e004cd5491955b5
Andy Gock <andy@gock.net>
2020-05-23 20:54:12 +1000

    Add copying to clipboard, some styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import NumberPicker from './NumberPicker';
 import WordListRadio from './WordListRadio';
+import '../node_modules/normalize.css/normalize.css';
 import './App.css';
 import Output from './Output';
 
@@ -32,30 +33,36 @@ const App = () => {
         <h1>Password Generator</h1>
       </header>
 
-      <p>Number of words per passphrase</p>
-      <NumberPicker
-        onChange={(val) => setWordsPerPassphrase(val)}
-        value={wordsPerPassphrase}
-      />
+      <div className="container">
+        <div className="col">
+          <h3>Number of words per passphrase</h3>
+          <NumberPicker
+            onChange={(val) => setWordsPerPassphrase(val)}
+            value={wordsPerPassphrase}
+          />
 
-      <p>Number of passphrases</p>
-      <NumberPicker
-        onChange={(val) => setNumberOfPassphrases(val)}
-        value={numberOfPassphrases}
-      />
+          <h3>Number of passphrases</h3>
+          <NumberPicker
+            onChange={(val) => setNumberOfPassphrases(val)}
+            value={numberOfPassphrases}
+          />
 
-      <p>Word list</p>
-      <WordListRadio value={wordlist} onChange={setWordlist} />
+          <h3>Word list</h3>
+          <WordListRadio value={wordlist} onChange={setWordlist} />
 
-      <div>
-        <button onClick={handleReset}>Reset to defaults</button>
+          <div>
+            <button onClick={handleReset}>Reset to defaults</button>
+          </div>
+        </div>
+
+        <div className="col">
+          <Output
+            words={wordsPerPassphrase}
+            lines={numberOfPassphrases}
+            list={wordlist}
+          />
+        </div>
       </div>
-
-      <Output
-        words={wordsPerPassphrase}
-        lines={numberOfPassphrases}
-        list={wordlist}
-      />
     </div>
   );
 };
--- src/Output.js
+++ src/Output.js
@@ -1,40 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import classNames from 'classnames';
 import dict from './words';
 
 const Output = ({ list, words, lines }) => {
-  // keep track how many times user is clicking re-generate
-  const [counter, setCounter] = React.useState(0);
+  const [copied, setCopied] = React.useState('');
+  const [rows, setRows] = React.useState([]);
 
-  const arr = new Uint32Array(lines * words);
-  const random = window.crypto
-    .getRandomValues(arr)
-    .map((v) => v % dict[list].length);
+  const generate = () => {
+    const arr = new Uint32Array(lines * words);
+    const random = window.crypto
+      .getRandomValues(arr)
+      .map((v) => v % dict[list].length);
 
-  // generate passphrases
-  let index = 0;
-  const rows = [];
-  for (let row = 0; row < lines; row++) {
-    const line = [];
-    for (let n = 0; n < words; n++) {
-      line.push(dict[list][random[index]]);
-      index += 1;
+    // generate passphrases
+    let index = 0;
+    const rows = [];
+    for (let row = 0; row < lines; row++) {
+      const line = [];
+      for (let n = 0; n < words; n++) {
+        line.push(dict[list][random[index]]);
+        index += 1;
+      }
+      rows.push(line);
     }
-    rows.push(line);
-  }
+
+    setRows(rows);
+    setCopied('');
+  };
+
+  React.useEffect(() => {
+    generate();
+  }, [list, words, lines]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const handleClick = () => {
+    // copy passphrase to clipboard
+  };
+
+  const handleCopy = (text, result) => {
+    if (result) {
+      setCopied(text);
+    }
+  };
 
   return (
-    <div>
+    <div className="">
       <button
         onClick={() => {
-          setCounter(counter + 1);
+          generate();
         }}
       >
         Regenerate
       </button>
-      {rows.map((row, rowNumber) => (
-        <div key={rowNumber}>{row.join(' ')}</div>
-      ))}
+      {copied !== '' && <span>*copied to clipboard*</span>}
+      {rows.map((row, rowNumber) => {
+        const pass = row.join(' ');
+        return (
+          <CopyToClipboard key={rowNumber} text={pass} onCopy={handleCopy}>
+            <div
+              className={classNames('pointer', { selected: pass === copied })}
+            >
+              <span>{pass}</span>
+            </div>
+          </CopyToClipboard>
+        );
+      })}
     </div>
   );
 };
