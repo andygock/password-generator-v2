380c79647e1e4a85e3a3373c56ac7270a53a082c
Andy Gock <andy@gock.net>
2020-05-24 21:08:18 +1000

    Add footer

--- src/App.js
+++ src/App.js
@@ -63,6 +63,25 @@ const App = () => {
           />
         </div>
       </div>
+
+      <footer>
+        <p>
+          Runs entirely in the web browser using{' '}
+          <a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Crypto_API">
+            Web Crypto API
+          </a>{' '}
+          and{' '}
+          <a href="https://www.eff.org/deeplinks/2016/07/new-wordlists-random-passphrases">
+            EFF word lists
+          </a>
+          . Passphrases are not stored or transmitted. Source code is available
+          on GitHub with MIT License.
+        </p>
+        <p>
+          &copy;
+          <a href="https://gock.net/">Andy Gock</a>
+        </p>
+      </footer>
     </div>
   );
 };
