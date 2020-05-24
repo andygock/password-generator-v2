a33999552c1e77168af3d3f67258dc4fba62c868
Andy Gock <andy@gock.net>
2020-05-24 21:06:55 +1000

    Update copy to clipboard behaviour

---
+++ src/CopiedToClipboard.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+
+const CopiedToClipboard = ({ text }) => {
+  const [opacity, setOpacity] = React.useState(0);
+
+  useEffect(() => {
+    // don't display when nothing is copied
+    if (text === '') return;
+
+    // make message visible, then fade out
+    setOpacity(1);
+    setTimeout(() => {
+      setOpacity(0);
+    }, 1000);
+  }, [text]);
+
+  return (
+    <span
+      style={{
+        transition: 'opacity 0.2s ease',
+        opacity: opacity,
+        backgroundColor: 'greenyellow',
+        padding: '0.2rem',
+      }}
+    >
+      Copied to clipboard
+    </span>
+  );
+};
+
+CopiedToClipboard.propTypes = {};
+
+CopiedToClipboard.defaultProps = {};
+
+export default CopiedToClipboard;
--- src/Output.js
+++ src/Output.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import copy from 'copy-to-clipboard';
 import classNames from 'classnames';
 import dict from './words';
+import CopiedToClipboard from './CopiedToClipboard';
 
 const Output = ({ list, words, lines }) => {
   const [copied, setCopied] = React.useState('');
-  const [rows, setRows] = React.useState([]);
+  const [passphrases, setPassphrases] = React.useState([]);
 
-  const generate = () => {
+  const generate = ({ lines, words, wordArray }) => {
     const arr = new Uint32Array(lines * words);
     const random = window.crypto
       .getRandomValues(arr)
-      .map((v) => v % dict[list].length);
+      .map((v) => v % wordArray.length);
 
     // generate passphrases
     let index = 0;
@@ -20,50 +21,47 @@ const Output = ({ list, words, lines }) => {
     for (let row = 0; row < lines; row++) {
       const line = [];
       for (let n = 0; n < words; n++) {
-        line.push(dict[list][random[index]]);
+        line.push(wordArray[random[index]]);
         index += 1;
       }
       rows.push(line);
     }
 
-    setRows(rows);
-    setCopied('');
+    return rows;
   };
 
   React.useEffect(() => {
-    generate();
+    const passes = generate({ lines, words, wordArray: dict[list] });
+    setPassphrases(passes);
+    setCopied('');
   }, [list, words, lines]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  const handleClick = () => {
-    // copy passphrase to clipboard
-  };
-
-  const handleCopy = (text, result) => {
-    if (result) {
-      setCopied(text);
-    }
+  const handleCopy = (text) => (e) => {
+    if (copy(text)) setCopied(text);
   };
 
   return (
     <div className="">
       <button
         onClick={() => {
-          generate();
+          const passes = generate({ lines, words, wordArray: dict[list] });
+          setPassphrases(passes);
+          setCopied('');
         }}
       >
         Regenerate
       </button>
-      {copied !== '' && <span>*copied to clipboard*</span>}
-      {rows.map((row, rowNumber) => {
+      <CopiedToClipboard text={copied} />
+      {passphrases.map((row, rowNumber) => {
         const pass = row.join(' ');
         return (
-          <CopyToClipboard key={rowNumber} text={pass} onCopy={handleCopy}>
-            <div
-              className={classNames('pointer', { selected: pass === copied })}
-            >
-              <span>{pass}</span>
-            </div>
-          </CopyToClipboard>
+          <div
+            key={pass}
+            className={classNames('pointer', { selected: pass === copied })}
+            onClick={handleCopy(pass)}
+          >
+            {pass}
+          </div>
         );
       })}
     </div>
