f5f416180ed8985b5b3f69f041b7b341a6657079
Andy Gock <andy@gock.net>
2022-09-11 00:20:36 +1000

    Bump deps

--- public/index.js
+++ public/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-
-import ReactDOM from 'react-dom';
-
+import { createRoot } from 'react-dom/client';
 import App from '../src/App';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const root = createRoot(rootElement);
+root.render(<App />);
